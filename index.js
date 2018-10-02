c45d1d0105361fcb23707490001db52628afa970
Kevin Huang <yellow.khaos@gmail.com>
2018-10-02 01:53:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 function scuberGreetingForFeet(distance) {
     let message
 
-    switch (distance) {
-      case distance <=400:
+      if distance <=400 {
         message = 'This one is on me!'
-        break
-      case distance > 2500:
+      }
+      else if (distance > 2500) {
         message = 'No can do.'
-        break
-      case distance > 2000:
+      }
+      else if (distance >2000) {
         message = 'I will gladly take your thirty bucks.'
-        break
+      }
+      
       return message
     }
 }
