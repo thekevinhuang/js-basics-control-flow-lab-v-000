7961dd0b5ea13cf993fc8a5adf3f28323c9bea37
Kevin Huang <yellow.khaos@gmail.com>
2018-10-02 02:24:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+    let message
+
+    if (distance <=400) {
+      message = 'This one is on me!'
+    }
+    else if (distance > 2500) {
+      message = 'No can do.'
+    }
+    else if (distance > 2000) {
+      message = 'I will gladly take your thirty bucks.'
+    }
+
+    return message
+}
+
+function ternaryCheckCity(city) {
+  return  city === "NYC" ? 'Ok, sounds good.' : 'No go.'
+}
+
+function switchOnCharmFromTip(tip) {
+  let message
+
+  switch(tip) {
+    case 'generous':
+      message = "Thank you so much."
+      break
+    case 'not as generous':
+      message = "Thank you."
+      break
+    default:
+      message = "Bye."
+    }
+    return message
+}
